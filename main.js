19e33dd9a1176401290902c3a949e65deb2c6750
your username in github <MalekWahmi>
2020-12-17 12:03:39 +0100

    init

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 $("#id01").hide()
-$("#BT1").click(function(){
+$(".b1").click(function(){
   $(".head").hide("fast")
+  
   $("#id01").show("slow")
 })
+
+
+// // $('.signupbtn').click(function(){
+//     //  document.getElementById("data").textContent = "firstName : " + $('#fname').val() + "\n " +" lastName : " + "\n " + $('#lname').val() + " \n" + "Address :"+ $("#address").val() + "\n " + "e-mail address :" + $("#address").val()
+//  $('#data').show()
+// data= $('#fname').val() + " " + $('#lname').val() + " " + $("#address").val() +  " " + $("#add").val()
+//  localStorage.setItem($('#fname').val(), data);
+// })
+
+// localStorage.setItem($('#fname').val , data)
