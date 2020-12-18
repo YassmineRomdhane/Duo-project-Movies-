a30fcc3af7c9e4090c0e2a86f513c7d09463403b
your username in github <MalekWahmi>
2020-12-18 17:13:53 +0100

    init

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
  $(".login").hide()
+  $("#found").hide()
+
+ 
+function searchMovies(){
+    $("#found").empty()
+
+  var name = $('#search').val()
+  console.log(name)
+  for(var i = 0 ; i<movies.length; i++){
+   if(movies[i].name.toUpperCase()===name.toUpperCase()){
+ $("#found").append("<div class='category1'>" +
+  "<img src=" + movies[i].image + " class='second'>" +
+ " <p>"+ movies[i].name + "</p>" +
+"</div>"
+);   
+$(".store").hide() 
+$(".store2").hide() 
+$("#slideShow").hide()
+$("#found").show()
+
+return;
+    }
+}
+alert("your movie is not found")
+}
 $("#BBT").click(function(){
   $(".head").hide("fast")
   $(".store").hide("fast")
-  
+  $(".favorite").hide() 
+  $("#slideShow").hide()
+  $(".store2").hide()
   $(".login").show("slow")
 })
 
-
-
+$("#cancel").click(function(){
+  $(".login").hide("slow")
+  $(".head").show("fast")
+  $(".store").show("fast")
+  $(".favorite").show() 
+  $("#slideShow").show()
+  $(".store2").show()
+  
+})
 $("#loginB").click(function(){
  console.log($("#inp").val());
  console.log($("#inp1").val());
  console.log($("#inp2").val());
-    
-       return false;
+    return false;
 });
 
 
@@ -56,33 +89,31 @@ var movies=[
      },
        {name: "Saw",
         image: "https://img.filmsactu.net/datas/films/s/a/saw-spiral/xl/saw-spiral-5e3ad0139f7c4.jpg",
-        dateOfPublication: "1 octobre 2004"
+        
      },
        {name:"Blow",
         image: "https://images-na.ssl-images-amazon.com/images/I/51ABCZ3C8TL._AC_.jpg",
-        dateOfPublication:"29 mars 2001"
+        
    
        },
        {name:"Takers",
         image: "https://images-na.ssl-images-amazon.com/images/I/816dc%2BxiIHL._AC_SY445_.jpg",
-        dateOfPublication: "4 août 2010"
+        
    
        },
   
        {name: "Project Power",
         image: "https://www.denofgeek.com/wp-content/uploads/2020/07/project-power-poster-netflix.jpg?fit=1200%2C680",
-        dateOfPublication: "14 août 2020"
+       
 
    
        },
        {name: "Suicide squad",
         image: "https://images-na.ssl-images-amazon.com/images/I/A1tDLvY6efL._AC_SY445_.jpg",
-        dateOfPublication: "1 août 2016"
        },
        {
         name:"divergent", 
         image: "https://fr.web.img2.acsta.net/pictures/15/01/28/18/08/152985.jpg",
-        dateOfPublication: "18 mars 2014"
        }
 ];
 
@@ -90,7 +121,7 @@ $("body").append("<ul></ul>");
 for (var i = 0; i < movies.length; i++) {
   $(".store").append("<div class='category1'>" +
   "<img src=" + movies[i].image + " class='second'>" +
- " <p>"+ movies[i].name + "</p>" +  "<input type='checkbox' id='vehicle1' name='vehicle1' value='Bike'></input>"+
+ " <p>"+ movies[i].name + "</p>" +  "<button type='button' id='check' onclick='pick(" + "movies[" + i + "].name" + ")' value='movies'> + </button>"+
 "</div>"
 );
 } 
@@ -98,40 +129,61 @@ for (var i = 0; i < movies.length; i++) {
      { 
        name: "frozen" , 
        image:"https://upload.wikimedia.org/wikipedia/en/0/05/Frozen_%282013_film%29_poster.jpg",
-      dateOfPublication: "19 novembre 2013"
+     
     },
     {
     name: "Ice age" , 
     image: "https://fr.web.img6.acsta.net/medias/nmedia/00/02/47/01/affiche.jpg" ,
-    dateofpublication: "12 mars 2002"
+
     },
     {
       name: "Nemo",
       image:"https://slspotlight.com/wp-content/uploads/2012/09/FindingNemo.jpg",
-      dateofpublication:"30 mai 2003"
+      
     },
-     
-     {name: "lion king",
-      image:"https://img01.mgo-images.com/image/thumbnail/v2/content/MMVFD4527132F1B898E30D0400B847E4C759.jpeg",
-      dateofpublication:"9 juillet 2019"
-
-   }
+     {
+     name: "Bolt",
+      image:"https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-13brs4i_abd7aa3a.jpeg?region=0%2C0%2C1000%2C1502",
+     }
    ];
 $("body").append("<ul></ul>");
 for (var i = 0; i < kids.length; i++) {
   $(".store2").append("<div class='category2'>" +
   "<img src=" + kids[i].image + " class='second2'>" +
- " <p>"+ kids[i].name + "</p>" +
+ " <p>"+ kids[i].name + "</p>" +"<button type='button' id='check' onclick='pick(" + "kids[" + i + "].name" + ")' value='movies'> + </button>"+
 "</div>"
 );
 }
+function pick(name){ 
+ var favoret = JSON.parse(localStorage.getItem('favoret'));
+ favoret.push(name)
+ localStorage.setItem('favoret', JSON.stringify(favoret));
+  favoret = JSON.parse(localStorage.getItem('favoret'));
+ console.log("this is favoret ==== >" , favoret)
+}
+$(".favorite").hide() 
+$(".BT").click(function(){
+   var favoret = JSON.parse(localStorage.getItem('favoret'));
 
+  $(".favorite").empty()
+for (var i = 0; i < favoret.length; i++) {
+     for(var j = 0 ; j < movies.length ; j++){
+       if(movies[j].name === favoret[i]){
+           $(".favorite").append("<div class='category1'>" +
+  "<img src=" + movies[j].image + " class='second'>" +
+ " <p>"+ movies[j].name + "</p>" +  
+"</div>"
+);
+       }
+     }  
+}
 
-
-
-
+$(".store").hide() 
+$(".store2").hide() 
+$("#slideShow").hide()
+$(".favorite").show() 
+}) 
 $('#data').hide()
-
 var users=[]
 $('#loginB').click(function(){
 if($('#inp1').val()===$("#inp2").val()){
@@ -149,29 +201,3 @@ else{
 
 
 
-
-//      document.getElementById("data").textContent = "E-mail:" + $('#inp').val() + "Password:"   + $('#inp1').val()  + "Repeat Password:"+ $("#inp2").val() 
-// data= $('#inp').val() + " " + $('#inp1').val() + " " + $("#inp2").val() 
-//  localStorage.setItem('users', data);
-
-
-
-
-
-
-
-
-
-// var name = $('#search).val()
-
-// function searchMovies(name,movies){
-  
-//   var result = "Your Movie is not found";
-//   for(var i = 0 ; i<movies.length; i++){
-//    if(movies[i].name.toUpperCase()===name.toUpperCase()){
-//   //  console.log("Found it !")
-//    console.log(movies[i]);
-//     }
-// }
-//     return result
-// }
